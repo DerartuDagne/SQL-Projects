6643980ac336b205c2866892fd49c81e3d4b5ead
DerartuDagne <112087783+DerartuDagne@users.noreply.github.com>
2022-12-15 22:58:17 +0300

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,8 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[RollingPeopleVaccinated]
+  FROM [Portfolioproject].[dbo].[PercentPopulationVaccinated]
